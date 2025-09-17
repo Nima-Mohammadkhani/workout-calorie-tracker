6b6d18bf5950d38f208fa944d79ea019558959d3
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-09-17 09:14:21 +0330

    add profile page

---
+++ workout-calorie-tracker/app/(tabs)/profile.tsx
@@ -0,0 +1,17 @@
+import ComingSoon from "@/components/ComingSoon";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <ComingSoon
+      title="پروفایل"
+      description="صفحه پروفایل شخصی شما در حال تکمیل است! به زودی می‌توانید آمار پیشرفت، دستاوردها، تنظیمات شخصی و بسیاری از ویژگی‌های دیگر را مشاهده کنید."
+      icon="person"
+      primaryColor="#38b2ac"
+      secondaryColor="#fed6e3"
+      accentColor="#4ecdc4"
+    />
+  );
+};
+
+export default Profile;
